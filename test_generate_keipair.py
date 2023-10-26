affab5e0c7d82a8f0cb1d13ef10664b92d1cfca0
Артем Корольков <144233569+ArtemKorolkov@users.noreply.github.com>
2023-10-26 11:58:15 +0300

    Delete test_generate_keipair.py

--- test_generate_keipair.py
+++
@@ -1,6 +0,0 @@
-from main_lab2 import generate_keypair
-import pytest
-
-@pytest.mark.parametrize('a,b,expected_result',[('wfd',5,"Incorrect input")])
-def test_calculator(a,b,expected_result):
-    assert generate_keypair(a,b) == expected_result
